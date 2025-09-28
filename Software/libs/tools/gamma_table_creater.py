92bed56ddd2f632d5e7e31247d0f325c4ec4355e
MaxWilli86 <lukas.bartels@smartcube360.de>
2025-09-28 21:46:45 +0200

    modified python script

--- Software/libs/tools/gamma_table_creater.py
+++ Software/libs/tools/gamma_table_creater.py
@@ -14,10 +14,15 @@ def init_gamma_table():
         gamma_table[i] = int(pow(i / 255.0, GAMMA) * 255.0 + 0.5)
     return gamma_table
 
+def create_gamma_table_string(gamma_table:list):
+    gamma_string = str(gamma_table)
+    gamma_string = gamma_string.replace('[', '')
+    gamma_string = gamma_string.replace(']', '')
+    return gamma_string
+   
 gamma_table = init_gamma_table()
-gamma_string = str(gamma_table)
-gamma_string = gamma_string.replace('[', '')
-gamma_string = gamma_string.replace(']', '')
+gamma_string = create_gamma_table_string(gamma_table)
+
 with open("gamma_table.txt", "w") as f:
   f.write("static uint8_t gamma_table[256] = { ")
   f.write(gamma_string)
